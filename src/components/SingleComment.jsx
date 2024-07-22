d7317be0ef752183fae73d3807924103d00d9a88
GiovanniDiMaria <158842385+GiovanniDiMaria96@users.noreply.github.com>
2024-07-22 20:20:24 +0200

    Add files via upload

---
+++ src/components/SingleComment.jsx
@@ -0,0 +1,39 @@
+import { Button, ListGroup } from 'react-bootstrap'
+
+const SingleComment = ({ comment }) => {
+  const deleteComment = async (asin) => {
+    try {
+      let response = await fetch(
+        'https://striveschool-api.herokuapp.com/api/comments/' + asin,
+        {
+          method: 'DELETE',
+          headers: {
+            Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjkzZTBkYTI4ZjI2ZDAwMTU1ZjJkODgiLCJpYXQiOjE3MjE2NzIyODYsImV4cCI6MTcyMjg4MTg4Nn0.oOMQL-LwJuuQgo3Ns52Mn8dzXeYHmqZQ5jRBeolSQWw",
+          },
+        }
+      )
+      if (response.ok) {
+        alert('La recensione è stata elimata!')
+      } else {
+        throw new Error('La recensione non è stata eliminata!')
+      }
+    } catch (error) {
+      alert(error)
+    }
+  }
+
+  return (
+    <ListGroup.Item data-testid="single-comment">
+      {comment.comment}
+      <Button
+        variant="danger"
+        className="ms-2"
+        onClick={() => deleteComment(comment._id)}
+      >
+        Elimina
+      </Button>
+    </ListGroup.Item>
+  )
+}
+
+export default SingleComment
